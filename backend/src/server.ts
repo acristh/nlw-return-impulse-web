c7ceb15c1c354cd1be566fa8fdddb76e648bbb2f
André Araújo <andre.cristiano1975@gmail.com>
2022-06-05 12:32:47 -0300

    configurada a porta do servico backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,6 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
-  console.log('Server running on Port 3333...');
+app.listen(process.env.PORT || 3333, () => {
+  console.log(`Server running on Port ${process.env.PORT || 3333}...`);
 });
